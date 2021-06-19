61567bed2d7455caf11b3132448803c1d1463815
krzgr <krzgr@DESKTOP-NOI1KH6.localdomain>
2021-06-19 13:17:12 +0200

    - added some testing functions

--- tests/test.cpp
+++ tests/test.cpp
@@ -87,6 +87,59 @@ TEST(chip_test, check_subroutines)
     t.clockCycle();
     ASSERT_EQ(t.getPC(), 0x555);
 
+    ASSERT_EQ(t.getSP(), 1);
+
     t.clockCycle();
     ASSERT_EQ(t.getPC(), 0x202);
+
+    ASSERT_EQ(t.getSP(), 0);
 }
+
+TEST(chip_test, jump_to_adress)
+{
+    CHIP8_test t;
+    //Jumps to 0x0555
+    uint8_t instr[] = { 0x15, 0x55 };
+
+    memcpy(&t.getRAM()[0] + t.getPC(), instr, sizeof(instr));
+
+    t.clockCycle();
+    ASSERT_EQ(t.getPC(), 0x555);
+}
+
+TEST(chip_test, skipping_instructions)
+{
+    CHIP8_test t;
+    //Skip next instruction if Vx == NN
+    uint8_t instr[] = { 0x30, 0x55, // skip if V[0] == 0x55
+                        0x30, 0x00, // skip if V[0] == 0x00
+                        0x00, 0x00, // empty instruction
+                        0x40, 0x00, // skip if V[0] != 0x00
+                        0x4f, 0xff, // skip if V[0xf] != 0xff
+                        0x00, 0x00, // empty instruction
+                        0x5e, 0xa0, // skip if V[0xe] == V[0xa]
+                        0x00, 0x00, // empty instruction
+                        0x91, 0x30 // skip if V[0x1] != V[0x3]
+                      };
+
+    memcpy(&t.getRAM()[0] + t.getPC(), instr, sizeof(instr));
+
+    t.clockCycle();
+    ASSERT_EQ(t.getPC(), 0x202);
+
+    t.clockCycle();
+    ASSERT_EQ(t.getPC(), 0x206);
+
+    t.clockCycle();
+    ASSERT_EQ(t.getPC(), 0x208);
+
+    t.clockCycle();
+    ASSERT_EQ(t.getPC(), 0x20c);
+
+    t.clockCycle();
+    ASSERT_EQ(t.getPC(), 0x210);
+
+    t.clockCycle();
+    ASSERT_EQ(t.getPC(), 0x212);
+}
+
